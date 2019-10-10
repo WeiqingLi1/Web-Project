f2e5466ce67e2df5f2f61fc8a960bd02ed8af10b
WeiqingLi1 <32884059+WeiqingLi1@users.noreply.github.com>
2019-10-09 22:33:05 -0400

    Eclipse Java Code uploaded
    Eclipse Java Code uploaded

---
+++ Jupiter/src/external/URL.java
@@ -0,0 +1,16 @@
+package external;
+
+import java.net.HttpURLConnection;
+
+public class URL {
+
+	public URL(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public HttpURLConnection openConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
